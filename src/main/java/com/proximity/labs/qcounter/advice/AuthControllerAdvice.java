4ed03904fe67064799e260004350c3f024262e0d
chathil <chathil98@gmail.com>
2020-07-19 19:05:14 +0700

    documentation

--- src/main/java/com/proximity/labs/qcounter/advice/AuthControllerAdvice.java
+++ src/main/java/com/proximity/labs/qcounter/advice/AuthControllerAdvice.java
@@ -11,6 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.proximity.labs.qcounter.advice;
 
 import org.apache.log4j.Logger;
--- src/main/java/com/proximity/labs/qcounter/data/repositories/UserRepository.java
+++ src/main/java/com/proximity/labs/qcounter/data/repositories/UserRepository.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findFirstByEmailAndPassword(String email, String password);
+    Optional<User> findFirstByEmailAndPassword(String email, String password);
 
     Optional<User> findById(Long id);
     
