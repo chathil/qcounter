2eccdb37ca9f04c3825011a1a8250a8e27e85578
chathil <chathil98@gmail.com>
2020-08-02 13:13:23 +0700

    fixing test dependency

---
+++ src/main/java/com/proximity/labs/qcounter/config/DatabaseConfig.java
@@ -0,0 +1,24 @@
+package com.proximity.labs.qcounter.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+
+@ConditionalOnProperty(value="otpConfig", havingValue="production")
+@Configuration
+public class DatabaseConfig {
+    private final String dbUrl = System.getenv("JDBC_DATABASE_URL");
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(dbUrl);
+        return new HikariDataSource(config);
+    }
+}
